0ba00b45997fddeb0bcb8dee33cdc2816e016814
connorbly1 <cpbly@ncsu.edu>
2025-06-23 20:24:52 -0400

    Removed unneeded code

--- movieRental/load.py
+++ movieRental/load.py
@@ -31,28 +31,3 @@ def load_data(customers, employees, movies, rentals):
     employees.to_sql("employees", con=engine, if_exists="append", index=False)
     movies.to_sql("movies", con=engine, if_exists="append", index=False)
     rentals.to_sql("rentals", con=engine, if_exists="append", index=False)
-
-
-
-
-
-'''
-employees = pd.read_csv("orgPipeline/employee_data2.csv")
-employees = clean_employee(employees)
-
-departments = pd.read_csv("orgPipeline/department_data.csv")
-projects = pd.read_csv("orgPipeline/projects.csv")
-assignments = pd.read_csv("orgPipeline/employee_project.csv")
-
-engine = create_engine(f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}")
-
-departments.to_sql("departments", con=engine, if_exists="append", index=False)
-employees.to_sql("employees", con=engine, if_exists="append", index=False)
-projects.to_sql("projects", con=engine, if_exists="append", index=False)
-
-assignments = assignments[assignments['employee_id'].isin(employees['employee_id'])]
-assignments.to_sql("assignments", con=engine, if_exists="append", index=False)
-
-df = pd.read_sql("SELECT * FROM employees LIMIT 5;", con=engine)
-print(df)
-'''
